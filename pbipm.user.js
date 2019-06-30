4e6fd60d24d8c28b790cfe101a011fd8b4be114f
Ivan Matvienko <ivan.matvienko@gmail.com>
2019-07-01 00:25:16 +0300

    Update pbipm.user.js

--- pbipm.user.js
+++ pbipm.user.js
@@ -2,7 +2,7 @@
 // @name         Power BI Password Manager
 // @namespace    https://github.com/vaaho/pbipm
 // @description  Autofill passwords when sign in Power BI
-// @version      0.1
+// @version      1.0.0
 // @license      MIT
 // @author       vaaho
 // @homepageURL  https://github.com/vaaho/pbipm
